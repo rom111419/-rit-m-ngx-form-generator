515428f412573a91fc545056b738c9cbc9aa12c4
Roman Zhukov <nortall.ritmby@gmail.com>
2022-05-07 02:47:49 +0300

    Release 0.0.3: ng library is created, figure, font, grid, media and scroll scss rules are added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Form generator
 
 Angular form generator creates reactive form groups, form controls and form arrays from Object
+
+    Changes log:
+      - run method description added
--- src/lib/form-generator.service.ts
+++ src/lib/form-generator.service.ts
@@ -10,6 +10,35 @@ export class FormGeneratorService {
   constructor(private fB: FormBuilder, private iF: ConditionService) {
   }
 
+  /**
+   * FormGeneratorService creates reactive form groups, form controls and form arrays from Object
+   * example:
+   * ```
+   *     Input = {
+   *      "key": [
+   *        "value",
+   *        ['arrayValue'],
+   *        {"fieldControl": "value", "fieldArray": ['arrayValue'], "fieldGroup": {"key": "anyValue"}}
+   *      ],
+   *      "fieldGroup": {"key": "primitiveOrArrayOrObject"}}
+   *     }
+   *     Output = FormGroup({
+   *        "key": FormArray([
+   *           FormControl('value'),
+   *           FormArray([FormControl('arrayValue')]),
+   *           FormGroup({
+   *               FormGroup({fieldControl: FormControl('value')}),
+   *               FormArray("fieldArray": [FormControl('arrayValue')]),
+   *               FieldGroup...
+   *             })
+   *         ]),
+   *         "fieldGroup": {"key": "primitiveOrArrayOrObject"}}
+   *        }
+   *     })
+   *
+   * @param {string} entitie some String or Array or Object;
+   * @param {string} fieldName field name for root nesting level. Not necessary.
+   * */
   public run(entitie: Record<string, unknown>, fieldName: string = ''): FormGroup | FormControl {
     return this.createGroupOrControl(entitie, fieldName);
   }
